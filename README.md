c3bff090ac7ce989503aceaa4a94e550c887e418
tegarajipangestu <13512061@std.stei.itb.ac.id>
2015-09-17 02:22:22 +0700

    Successfully crawled if students

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sixakademik-kepo
 A repository for kepoing sixakademik itb
+
+
+https://six.akademik.itb.ac.id/publik/displayprodijadwal.php?semester=1&tahun=2015&th_kur=2013
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -17,6 +20,41 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/kepo/:id', function(req, res){
+
+		kodekuliah = req.params.id;
+
+		templateurl = 'https://six.akademik.itb.ac.id/publik/'
+    daftarkelasurl = 'daftarkelas.php?ps=135&semester=1&tahun=2015&th_kur=2013'
+
+    request(templateurl+daftarkelasurl, function(error, response, html){
+
+        if(!error){
+            var $ = cheerio.load(html);
+
+            $('ol').children('li').each(function(index){
+            	var text = $(this).contents().filter(function(){ 
+							  return this.nodeType == 3; 
+							})[0].nodeValue;
+							// console.log(text);
+
+            	if (text.substr(0, text.indexOf(" ")).toLowerCase() === kodekuliah.toLowerCase())
+            	{
+								console.log(kodekuliah);
+								console.log(text.substr(0, text.indexOf(" ")).valueOf());
+            		link = $(this).find('ul > li:first-child > a').attr('href');
+            		console.log(templateurl+link);
+            		res.send(templateurl+link);
+            	}
+
+            });
+
+            res.send("Not found");
+
+        }
+
+    })
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
